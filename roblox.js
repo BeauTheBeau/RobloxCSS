0fced30c896252b6f2797c4cf2176ea0dbe2bb14
BeauTheBeau <90942493+BeauTheBeau@users.noreply.github.com>
2022-06-26 19:35:49 +0100

    Update roblox.js

--- roblox.js
+++ roblox.js
@@ -1,9 +1,6 @@
-// SETTINGS - set to true or false depending on your preference
-loadCSS = true // Loads updates CSS via the 69 Studios website 
-cleanTitle = true // Sets title to "Roblox"
-activity = true // Keeps track of your total time spent on the Roblox website
-
-refreshTime = 1000 // How often the setInterval() should run in milliseconds
+// SETTINGS
+loadCSS = true // Loads CSS if true
+cleanTitle = false // Sets title to "Roblox"
 
 // MAIN
 // functions
@@ -16,6 +13,8 @@ if (loadCSS == true) {
 	$('head').append('<link rel="stylesheet" type="text/css" href="https://69-studios.repl.co/exStyles/roblox.css">'); // Adds the CSS
 }
 
+
+
 // Logs time spent in Roblox
 var time = 0
 
@@ -24,15 +23,16 @@ if (localStorage.getItem('time') != null) {
 }
 
 setInterval(function () {
+	console.log(document.title)
+	
 	if (document.title != "Roblox" && cleanTitle == true) {document.title = "Roblox";}
 	time += 1
-	if (activity == true) {
-		if (time > 60) {
-			console.log(roundToTwo(time/60), " minutes or ", time, " seconds")
-		}
-		else {
-			console.log(time)
-		}
-		localStorage.setItem('time', time);
+	if (time > 60) {
+		console.log(roundToTwo(time/60), " minutes or ", time, " seconds")
 	}
-}, refreshTime); // runs every refreshTime ms
+	else {
+		console.log(time)
+	}
+	// console.log(localStorage.getItem('time'));
+	localStorage.setItem('time', time);
+}, 1000);
